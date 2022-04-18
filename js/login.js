1b204b92d289ba87e18a3bcd2e85f8c92fcb1919
Denis Githuku <githukudenis@gmail.com>
2022-04-18 23:09:52 +0300

    attached link to get started button

--- js/login.js
+++ js/login.js
@@ -1,37 +1,37 @@
-const loginBtn = document.getElementById('loginBtn');
+const loginBtn = document.getElementById("loginBtn");
 
-let userName = document.getElementById('user-link');
+let userName = document.getElementById("user-link");
 // userName.innerHTML = 'test100'
-loginBtn.addEventListener('click', () => {
-    let email = document.getElementById('email').value;
-    let pass = document.getElementById('pass').value;
-    if( email && pass ) {
-        firebase.auth().signInWithEmailAndPassword(email,pass)
-            .then((result) =>{
-                
-                 if ( result.user.emailVerified ) {
-                    window.alert('Login successful, redirecting...');
-                    console.log(result.user)
-                    window.location.assign('therapist-admin/index.html');
-                 }
-                 else {
-                     alert(`The email ${ result.user.email } is not verified`);
-                 }
-            })
-            .catch(err => alert(err.message));
-            
-    }
-    else {
-        window.alert('Both fields are required');
-    }
-})
+loginBtn.addEventListener("click", () => {
+  let email = document.getElementById("email").value;
+  let pass = document.getElementById("pass").value;
+  if (email && pass) {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, pass)
+      .then((result) => {
+        if (result.user.emailVerified) {
+          window.alert("Login successful, redirecting...");
+          console.log(result.user);
+          window.location.assign("../User/index.html");
+        } else {
+          alert(`The email ${result.user.email} is not verified`);
+        }
+      })
+      .catch((err) => alert(err.message));
+  } else {
+    window.alert("Both fields are required");
+  }
+});
 
 const googleBtn = document.getElementById("googleBtn");
 
-googleBtn.addEventListener('click',() => {
-    const authProvider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(authProvider)
-        .then(() => {
-            window.location.assign('home.html');
-        })
-})
+googleBtn.addEventListener("click", () => {
+  const authProvider = new firebase.auth.GoogleAuthProvider();
+  firebase
+    .auth()
+    .signInWithPopup(authProvider)
+    .then(() => {
+      window.location.assign("home.html");
+    });
+});
