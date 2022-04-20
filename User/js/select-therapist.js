0528249591a3a50cdd768a83f592dab8eaadf0a5
ZachyDev <mosetizachary001@gmail.com>
2022-04-20 19:55:19 +0300

    added book appointment feature

--- User/js/select-therapist.js
+++ User/js/select-therapist.js
@@ -1,20 +1,19 @@
-
 const dbRef = firebase.firestore();
-const selectTheraspist = document.getElementById('select-therapist');
+const selectTherapist = document.getElementById('selectTherapist');
 
 // get therapist name
 const getTherapist = doc => {
-    let therapistName = doc.data().name;
+    let therapistName = doc.data().fullName;
     
     // create option elements
     let nameOption = document.createElement('option');
     nameOption.innerHTML = therapistName;
 
-    selectTheraspist.appendChild(nameOption);
+    selectTherapist.appendChild(nameOption);
 }
 
     // fetch from firestore
-    let colRef = dbRef.collection('profileDetails');
+    let colRef = dbRef.collection('therapists');
     colRef.get()    
         .then(snapshot => {
             snapshot.forEach(doc => {
@@ -23,3 +22,29 @@ const getTherapist = doc => {
         })
         .catch(err => alert(err.message))
 
+// book appointmemt
+const bookAppointment = document.getElementById('bookAppointment');
+
+bookAppointment.addEventListener('submit', e => {
+    e.preventDefault();
+    let fullName = bookAppointment.name.value;
+    let email = bookAppointment.email.value;
+    let service = bookAppointment.service.value;
+    let phone = bookAppointment.phone.value;
+    let appointmentDate = bookAppointment.date.value;
+    let therapistSelect = bookAppointment.therapistSelect.value;
+
+    dbRef.collection('appointments').add({
+        fullName,
+        email,
+        service,
+        phone,
+        appointmentDate,
+        therapistSelect
+    })
+    .then(() => {
+        alert('Appointment has been booked  successfully');
+        bookAppointment.reset();
+    })
+    .catch(err => alert(err.message));
+})
